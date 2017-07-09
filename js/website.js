b20134a867446126d5a4bcde18e19383e531134a
Kelly <kdickerson149@gmail.com>
2017-07-09 13:31:46 -0400

    Portfolio overhaul

--- js/website.js
+++ js/website.js
@@ -16,68 +16,68 @@ const test = document.querySelector(".test");
 
 biggerOne.addEventListener("mouseover", (event) => {
 	if (event.target.id == "biggerOne") {
-	biggerOne.style.width = "210px";
-	biggerOne.style.height = "210px";
+	biggerOne.style.width = "410px";
+	biggerOne.style.height = "410px";
 	}
 });
 biggerOne.addEventListener("mouseout", () => {
-	biggerOne.style.width = "200px";
-	biggerOne.style.height = "200px";
+	biggerOne.style.width = "400px";
+	biggerOne.style.height = "400px";
 });
 
 biggerTwo.addEventListener("mouseover", (event) => {
 	if (event.target.id == "biggerTwo") {
-		biggerTwo.style.width = "210px";
-		biggerTwo.style.height = "210px";
+		biggerTwo.style.width = "410px";
+		biggerTwo.style.height = "410px";
 	}
 });
 biggerTwo.addEventListener("mouseout", () => {
-	biggerTwo.style.width = "200px";
-	biggerTwo.style.height = "200px";
+	biggerTwo.style.width = "400px";
+	biggerTwo.style.height = "400px";
 });
 
 biggerThree.addEventListener("mouseover", (event) => {
 	if (event.target.id == "biggerThree") {
-	biggerThree.style.width = "210px";
-	biggerThree.style.height = "210px";
+	biggerThree.style.width = "410px";
+	biggerThree.style.height = "410px";
 	}
 });
 biggerThree.addEventListener("mouseout", () => {
-	biggerThree.style.width = "200px";
-	biggerThree.style.height = "200px";
+	biggerThree.style.width = "400px";
+	biggerThree.style.height = "400px";
 });
 
 biggerFour.addEventListener("mouseover", (event) => {
 	if (event.target.id == "biggerFour") {
-	biggerFour.style.width = "210px";
-	biggerFour.style.height = "210px";
+	biggerFour.style.width = "410px";
+	biggerFour.style.height = "410px";
 	}
 });
 biggerFour.addEventListener("mouseout", () => {
-	biggerFour.style.width = "200px";
-	biggerFour.style.height = "200px";
+	biggerFour.style.width = "400px";
+	biggerFour.style.height = "400px";
 });
 
 biggerFive.addEventListener("mouseover", (event) => {
 	if (event.target.id == "biggerFive") {
-	biggerFive.style.width = "210px";
-	biggerFive.style.height = "210px";
+	biggerFive.style.width = "410px";
+	biggerFive.style.height = "410px";
 	}
 });
 biggerFive.addEventListener("mouseout", () => {
-	biggerFive.style.width = "200px";
-	biggerFive.style.height = "200px";
+	biggerFive.style.width = "400px";
+	biggerFive.style.height = "400px";
 });
 
 biggerSix.addEventListener("mouseover", (event) => {
 	if (event.target.id == "biggerSix") {
-	biggerSix.style.width = "210px";
-	biggerSix.style.height = "210px";
+	biggerSix.style.width = "410px";
+	biggerSix.style.height = "410px";
 	}
 });
 biggerSix.addEventListener("mouseout", () => {
-	biggerSix.style.width = "200px";
-	biggerSix.style.height = "200px";
+	biggerSix.style.width = "400px";
+	biggerSix.style.height = "400px";
 });
 
 
@@ -119,6 +119,49 @@ linkedin.addEventListener("mouseover", (event) => {
 linkedin.addEventListener("mouseout", () => {
 	linkedin.style.width = "60px";
 	linkedin.style.height = "60px";
+});
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+  // array with texts to type in typewriter
+  var dataText = [ "Software engineer.", "Experience in HTML, CSS, JavaScript and JQuery, Python, and iOS mobile development with Swift"];
+
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    if (i < (text.length)) {
+      // add next character to the text
+     document.querySelector("#animate").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+      // wait then call func again for next sentence
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 800);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 40000);
+     }
+     // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+     typeWriter(dataText[i], 0, function(){
+       // after callback (and whole text has been animated), start next text
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
 });
 
 
